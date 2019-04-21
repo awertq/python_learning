2d98eca9f7fc8f9da17eae6daaa579246fb13a84
awertq <awert2684@gmail.com>
2019-04-21 13:47:56 +0530

    added error.py file

---
+++ error.py
@@ -0,0 +1,23 @@
+'''trying try and except block '''
+
+a = 1
+
+
+try:
+	b = int(input("enter a num : "))
+	print(a/b)
+
+except ValueError as v:
+	print("input correct value sir ")
+
+except TypeError as t:
+	print("you get an type error ")
+
+except ZeroDivisionError as z:
+	print("do not input zero ")
+
+except Exception as e :
+	print("you got some error and that is ",e)
+
+finally:
+	print("at end every thing go right")
